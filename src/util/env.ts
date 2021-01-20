50ae869989fffc0e7667a1439f83341db9304c88
Riley Smith <riley@rileysmith.me>
2021-01-19 18:57:13 -0800

    Fixed some issues

--- src/util/env.ts
+++ src/util/env.ts
@@ -1,6 +1,8 @@
 import * as dotenv from "dotenv";
+import { resolve } from "path";
 dotenv.config();
-const path = `${__dirname}/../../../.env/`;
+const path = `${__dirname}/../../.env`;
+// console.log(resolve(path));
 dotenv.config({ path: path });
 
 export const discordToken = process.env.DISCORD_TOKEN;
